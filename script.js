50513c426e26c3330f366ae71daa146aef414cac
GBN6 <czesiox8@gmail.com>
2022-05-11 17:55:13 +0200

    Create basic grind and hover effect

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const container = document.querySelector('#wholePage');
 
 
-
+// function creating grid of divs from 0 to 255
 function creatGrid()
 {
     for (let i = 0; i < 256; i++)
     {
         const div = document.createElement('div');
         div.classList.add('gridBox');
+        div.setAttribute('id', 'div' + i)
         container.appendChild(div);
     }
+}
+
+function colorChange(e) {
+    e.target.style.backgroundColor = 'yellow';
 }
 
 creatGrid();
 
 
-const boxHover = document.querySelector('.gridBox')
-boxHover.addEventListener('click', (e) => {console.log(e)});
+const boxHover = document.querySelectorAll('.gridBox');
+boxHover.forEach(element => element.addEventListener('mouseover', colorChange));
+
+
+
