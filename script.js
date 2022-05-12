07b1458c0c2f66a00ed5b96903b0a9681cdfe856
GBN6 <czesiox8@gmail.com>
2022-05-12 17:11:16 +0200

    Create button that make new grid
    I've created a button that ask for users
    number of squares per side. After that the
    function clears existing grid and creates a new one
    Color hovering still working.

--- script.js
+++ script.js
@@ -1,27 +1,76 @@
 const container = document.querySelector('#wholePage');
-
+const gridBtn = document.querySelector('#btn-generete-grid');
+const gridBtnWarning = document.querySelector('.warning');
 
 // function creating grid of divs from 0 to 255
 function creatGrid()
 {
-    for (let i = 0; i < 256; i++)
+    clearGrid();
+    let x = getNumberSquares()
+    let y = x * x;
+    // while (container.firstChild) {
+    //     container.removeChild(container.firstChild);
+    // }
+    for (let i = 0; i < y; i++)
     {
         const div = document.createElement('div');
         div.classList.add('gridBox');
         div.setAttribute('id', 'div' + i)
         container.appendChild(div);
+        container.style.gridTemplateColumns = `repeat(${y/x}, auto [col-start])`;
+        const boxHover = document.querySelectorAll('.gridBox');
+        boxHover.forEach(element => element.addEventListener('mouseover', colorChange));
     }
 }
 
+//function to remove children nodes
+function clearGrid()
+{
+    while (container.firstChild) 
+    {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// function to change colors
 function colorChange(e) {
     e.target.style.backgroundColor = 'yellow';
 }
 
-creatGrid();
+
+//function to get squares number from user
+function getNumberSquares()
+{
+    let keepGoing = true;
+    while(keepGoing)
+    {
+        let numberSquares = parseInt(prompt('Please write how many number of squares per side show grid have. Max 100', ));
+        console.log(numberSquares);
+        if (Number.isInteger(numberSquares) && numberSquares <= 100)
+        {
+            return numberSquares;
+            keepGoing = false;
+        }
+        else if (Number.isNaN(numberSquares))
+        {
+            keepGoing = false;
+        }
+        else if (numberSquares > 100 || !Number.isInteger(numberSquares) || numberSquares === '')
+        {
+            alert('You didn\'t input number or it was higher than 100!');
+        }
+
+    }
+    
+
+}
 
 
+
+gridBtn.addEventListener('click', creatGrid);
 const boxHover = document.querySelectorAll('.gridBox');
 boxHover.forEach(element => element.addEventListener('mouseover', colorChange));
+console.log(boxHover);
 
 
 
